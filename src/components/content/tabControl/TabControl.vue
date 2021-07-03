84ed072ac010c847cdb9acb716e8b6236afb3abc
liux <1159469891@qq.com>
2021-07-03 18:14:52 +0800

    添加了首页商品数据的获取

---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="tab-control">
+    <div v-for="(item, index) in title"
+    :key="index"
+    class="tab-control-item"
+    :class="{active: index === currentIndex}"
+    @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'TabControl',
+    props: {
+      title: {
+        type: Array,
+        default() {
+          return []
+        }
+      }
+    },
+    data() {
+      return {
+        // 定义一个变量，记录谁被选中
+        currentIndex: 0
+      }
+    },
+    methods: {
+      itemClick(index) {
+        this.currentIndex = index;
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  .tab-control {
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    height: 40px;
+    line-height: 40px;
+  background-color: #fff;
+  }
+  .tab-control-item {
+    flex: 1;
+  }
+  .tab-control-item span {
+    padding: 5px;
+  }
+  .active {
+    color: var(--color-high-text);
+  }
+  .active span {
+    border-bottom: 3px solid #fb7d95;
+  }
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,25 +4,73 @@
     <home-swiper :banners="banners"></home-swiper>
     <home-recommend-view :recommends="recommends"></home-recommend-view>
     <home-feature-view></home-feature-view>
+    <tab-control class="tab-control" :title="['流行', '新款', '精选']"></tab-control>
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>4</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li>0</li>
+    </ul>
   </div>
 </template>
 
 <script>
+
+  import HomeSwiper from './childComps/HomeSwiper'
+  import HomeRecommendView from './childComps/HomeRecommendView'
+  import HomeFeatureView from './childComps/HomeFeatureView'
+
   import NavBar from 'components/common/navbar/NavBar'
-  import HomeSwiper from './childComps/HomeSwiper.vue'
-  import HomeRecommendView from './childComps/HomeRecommendView.vue'
-  import HomeFeatureView from './childComps/HomeFeatureView.vue'
-  import {getHomeMultidata} from 'network/home'
+  import TabControl from 'components/content/tabControl/TabControl'
+
+  import {getHomeMultidata, getHomeGoods} from 'network/home'
 
   // import {Swiper, SwiperItem} from 'components/common/swiper'
 
   export default {
     name: 'Home',
     components: {
-      NavBar,
       HomeSwiper,
       HomeRecommendView,
       HomeFeatureView,
+      NavBar,
+      TabControl,
     },
     data() {
       return {
@@ -31,17 +79,42 @@
         //存具体数据
         banners: [],
         recommends: [],
+        goods: {
+          'pop': {page: 0, list: []},
+          'new': {page: 0, list: []},
+          'sell': {page: 0, list: []},
+        }
       }
     },
     // 生命周期函数 创建完后马上执行
     created() {
-      // 1.请求多个数据
-      getHomeMultidata().then(res => {
-        console.log(res);
-        // this.result = res;
-        this.banners = res.data.banner.list;
-        this.recommends = res.data.recommend.list;
-      })
+      // 1.请求多个数据 加this 才是methods中第一个封装
+      this.getHomeMultidata()
+      // 2.请求商品数据
+      this.getHomeGoods('pop')
+
+      this.getHomeGoods('new')
+      
+      this.getHomeGoods('sell')
+    },
+    methods: {
+      getHomeMultidata() {
+        getHomeMultidata().then(res => {
+          // console.log(res);
+          // this.result = res;
+          this.banners = res.data.banner.list;
+          this.recommends = res.data.recommend.list;
+        })
+      },
+      getHomeGoods(type) {
+        const page = this.goods[type].page + 1;
+        getHomeGoods(type, page).then(res => {
+        // console.log(res);
+          // this.goods[type].list = this.goods[type].list.push(res.data.list);
+          this.goods[type].list.push(...res.data.list);
+          this.goods[type].page += 1;
+        })
+      }
     },
   }
 </script>
@@ -60,5 +133,10 @@
     left: 0;
     right: 0;
     z-index: 9;
+  }
+  .tab-control {
+    /* 粘性定位 */
+    position: sticky;
+    top: 44px;
   }
 </style>
